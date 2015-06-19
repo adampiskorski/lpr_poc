97fc195dc21b71ca16ac6e977a2163d7694f3fea
Phoenix <adam@piskorski.org>
2015-06-19 03:54:40 +0200

    Do PEP8 adjustments

--- test.py
+++ test.py
@@ -17,20 +17,20 @@ GPIO.setwarnings(False)
 # Set the pin to be an input
 GPIO.setup(LPR_PIN, GPIO.IN)
 
-# Only save the image once per gate openning
+# Only save the image once per gate opening
 captured = False
 # Main loop
 while True:
 
     # Capture the image if not captured yet and switch is closed (open gate)
-    if not captured and GPIO.input(LPR_PIN) == True:
+    if not captured and GPIO.input(LPR_PIN) is True:
         urllib.urlretrieve(SOURCE, FILE)
         print "Gate has been opened!"
         captured = True
 
     # If there was a capture and the switch is now open (closed gate) then
     # ready the loop to capture again.
-    if captured and GPIO.input(LPR_PIN) == False:
+    if captured and GPIO.input(LPR_PIN) is False:
         print "The gate has now closed!"
         captured = False
 
